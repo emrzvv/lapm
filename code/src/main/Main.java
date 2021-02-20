c08cb9a33ac7f20eb46cfb1adc571a3e6b20b29d
emrzvv <e.mrzvv@gmail.com>
2021-02-20 13:39:44 +0300

    first lecture

---
+++ code/src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Example e = new Example();
+        e.change_a_2(2);
+        Example.change_a(3);
+        //e.change_a(3);
+    }
+}
