7ee6c4a3c91e504dc5378565c0f29e77b3cfdfbf
Egor Porshenko <e.mrzvv@gmail.com>
2021-04-04 16:30:13 +0300

    1st task

--- code/05_lab/src/main/Main.java
+++ code/05_lab/src/main/Main.java
@@ -1,8 +1,25 @@
 package main;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String> lines = new ArrayList<>(Arrays.asList("aba", "abacaba", "aba", "abc", "abacaba", "zzz"));
+        ArrayList<Stream<String>> streams = new ArrayList<>();
+
+        HashSet<String> checker = new HashSet<>();
+
+        lines.forEach((line) -> {
+            if (!checker.contains(line)) {
+                checker.add(line);
+                streams.add(lines.stream().filter(line::equals));
+            }
+        });
+
+        streams.forEach((s) -> {
+            s.forEach((toPrint) -> {System.out.print(toPrint + " ");});
+            System.out.println();
+        });
     }
 }
